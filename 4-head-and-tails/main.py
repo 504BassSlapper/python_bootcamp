7f40544ea481aba6568fbabfc7a4d1df00eace9b
504BassSlapper <504bassslapper@gmail.com>
2023-06-15 07:02:01 +0200

    roulette exercice

---
+++ 4-head-and-tails/main.py
@@ -0,0 +1,7 @@
+import random
+
+random_side= random.randint(0,1)
+if random_side == 1:
+    print(f"{random_side} is the genereated number  -> Heads")
+else:
+    print(f"{random_side} is the genereated number Tails")
