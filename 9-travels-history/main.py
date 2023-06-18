b212c8e24bc6eab11140010efb3c0120d4745a78
504BassSlapper <504bassslapper@gmail.com>
2023-06-18 14:36:42 +0200

    fix formating

--- 9-travels-history/main.py
+++ 9-travels-history/main.py
@@ -14,38 +14,38 @@ travel_log = [
 wish_list_travel = [
     {
         "country": "France",
-        "priority": "1" ,
+        "priority": "1",
         "cities": ["Marseille", "Nice"],
     },
     {
         "country": "Danemark",
-        "priority" : "2",
+        "priority": "2",
         "cities": []
     }
 ]
+
 
 def add_travel(country, city):
     new_country = True
     for travel in travel_log:
         if travel["country"] in country:
             new_country = False
-            travel["visits"] = str(int(travel[1])+1)
+            travel["visits"] = str(int(travel[1]) + 1)
             if city not in travel["cities"]:
                 travel[2].add(city)
-                    
-    if new_country == True:
+
+    if new_country:
         add_country(country, city)
     print(f"{travel_log}")
-            
 
-def add_country(country,city):
+
+def add_country(country, city):
     country_dictionnary = {
-        "country":  country,
-        "visits" : "1",
+        "country": country,
+        "visits": "1",
         "cities": [city]
     }
     travel_log.append(country_dictionnary)
-    
-add_travel("Russia", "Moscou")
 
- 
+
+add_travel("Russia", "Moscou")
