0cbf8358612894f7fcdf94354f45239ded403217
504BassSlapper <504bassslapper@gmail.com>
2023-06-21 18:33:55 +0200

    turtle exercice

--- 18-turtle/main.py
+++ 18-turtle/main.py
@@ -1,8 +1,24 @@
 from turtle import Turtle, Screen
+
+
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("red")
 turtle.forward(100)
 
 screen = Screen()
+screen.setup(1000,1000)
 screen.exitonclick()
+
+# from turtle import Turtle, Screen
+
+# screen = Screen()
+# screen.setup(1000, 1000)
+# screen.title("Snake game")
+# screen.bgcolor("black")
+# screen.exitonclick()
+
+# segment_0 = Turtle(shape="square")
+# segment_0.color("white")
+
+
