b4e13d2ea81d16ecebfedfcdf0248c16f54de3d2
504BassSlapper <504bassslapper@gmail.com>
2023-07-01 13:06:04 +0200

    update

--- 38-google-sheet/flight_search.py
+++ 38-google-sheet/flight_search.py
@@ -1,6 +1,7 @@
+import os 
 
 TEQUILLA_ENDPOINT = "https://api.tequila.kiwi.com"
-TEQUILLA_API_KEY ="FqIjLyhtlPvlNewUbzwyArZeIpfvz8LO"
+TEQUILLA_API_KEY = os.getenv("TEQUILLA_API_KEY")
 class FlightSearch:
      
     def get_destination_code(self, city_name):
