8230bb4b5f702f85409b6a69c000f2fd3e97207f
504BassSlapper <504bassslapper@gmail.com>
2023-08-08 19:51:46 +0200

    add multiple parms function

--- 54-flask/Hello/hello.py
+++ 54-flask/Hello/hello.py
@@ -11,5 +11,16 @@ def hello():
 def bye(user):
     return f"bye {user}"
 
+@app.route("/username/<string:name>/<int:number>")
+def description(name, number ):
+    if number > 2:
+        return f"hello {name} you are the  {number} th client "
+    elif number == 2:
+        return f"hello {name} you are the {number} nd client "
+    elif number == 1:
+        return f"hello {name} you are the {number} st client <3"
+    else: 
+        return f"hello {name}"
+
 if __name__ =="__main__":
     app.run(debug=True)
